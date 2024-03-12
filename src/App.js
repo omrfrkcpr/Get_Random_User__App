fbd7868e58695001741e0adf354daea32f40368b
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-12 11:08:13 +0100

    initial: create project folder-file structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;

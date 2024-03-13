2ff80de8ccdd7cf768da91fbd216e5e7e4a34103
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-13 21:59:03 +0100

    fix bugs: (case-sensitive) transform to upperCase

--- src/components/editUser/EditUser.jsx
+++ src/components/editUser/EditUser.jsx
@@ -5,28 +5,28 @@ const EditUser = ({ item, setItem, editUser }) => {
 
   return (
     <div
-      class="modal fade"
+      className="modal fade"
       id="editModal"
       data-bs-backdrop="false"
       data-bs-keyboard="false"
-      tabindex="-1"
+      tabIndex="-1"
       aria-labelledby="exampleModalLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title fs-5" id="exampleModalLabel">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title fs-5" id="exampleModalLabel">
               Edit User
             </h5>
             <button
               type="button"
-              class="btn-close"
+              className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
           </div>
-          <div class="modal-body">
+          <div className="modal-body">
             {/* inputs */}
             <div className="mb-3">
               <label htmlFor="name">Name</label>
@@ -73,10 +73,10 @@ const EditUser = ({ item, setItem, editUser }) => {
               />
             </div>
           </div>
-          <div class="modal-footer">
+          <div className="modal-footer">
             <button
               type="button"
-              class="btn btn-success"
+              className="btn btn-success"
               data-bs-dismiss="modal"
               onClick={() => editUser(item)}
             >
